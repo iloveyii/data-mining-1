dd7143f15f9abcc8e5c8b47eb2aa645598eb5a49
Hazrat Ali <iloveyii@yahoo.com>
2019-12-24 12:43:01 +0100

    Added dataset from file

--- fis.py
+++ fis.py
@@ -7,12 +7,16 @@ dataset = [['A', 'B'],
            ['A', 'B', 'C', 'D'],
            ['A', 'E'],
            ['A', 'B', 'E']]
+# Open dataset from file
+fileName = './kosarak.dat'
+lineList = [line.rstrip('\n') for line in open(fileName) if not line.isspace()]
+print(lineList[0:5])
 
 te = TransactionEncoder()
-te_ary = te.fit(dataset).transform(dataset)
+te_ary = te.fit(lineList).transform(lineList)
 # print(te_ary)  # true false
 # print(te_ary.astype("int"))
 df = pd.DataFrame(te_ary, columns=te.columns_)
 # print(df)
-frequent_itemsets = apriori(df, min_support=0.4, use_colnames=True)
+frequent_itemsets = apriori(df, min_support=0.5, use_colnames=True)
 print(frequent_itemsets)
