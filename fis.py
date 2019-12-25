3d3856c309031dc378125802f7574b02837f6a72
Hazrat Ali <iloveyii@yahoo.com>
2019-12-25 15:04:12 +0100

    Averaing rows

--- fis.py
+++ fis.py
@@ -1,33 +1,72 @@
 from mlxtend.frequent_patterns import apriori
 from mlxtend.preprocessing import TransactionEncoder
 import pandas as pd
+import math
 
 average = []
 
-
-def addaverage(series):
-    print(series)
-    supportList = series['support'].tolist()
-    itemsetsList = series['itemsets'].tolist()
-    print(supportList, itemsetsList)
-    avg = []
-    val=[]
-    for item in itemsetsList:
-        print(str(item))
-        if not item in avg:
-            avg.append(item)
+# # dropping null value columns to avoid errors
+# data.dropna(inplace = True)
+dic = {}
+MIN_SUPP = 0.000001
 
 
-    print(avg)
-    # check if set exists
-    # add average
+def addaverage(frequent_itemsets):
+    global dic
+    for index, row in frequent_itemsets.iterrows():
+        itemsets = row['itemsets']
+        support = row['support']
+        print(support, itemsets)
+        continue
+        if itemsets in dic:
+            print('exist')
+            # take average
+            oldSupport = dic[itemsets]
+            print('oldSupport', oldSupport)
+            avgSupport = (oldSupport + support) / 2
+            dic[itemsets] = avgSupport
+        else:
+            dic[itemsets] = support
+        print(dic)
 
 
 def fis(dataset, split=1000):
+    global MIN_SUPP
     te = TransactionEncoder()
-    rowsCount = len(dataset)
+    rows_count = len(dataset)
+    max = rows_count
+    print('Max : ', max, range(0, max, split))
+    dic = {}
+    for count in range(0, max, split):
+        print(count)
+        subset = dataset[count:count + split]
+        print(subset)
+        te_ary = te.fit(subset).transform(subset)
+        # print(te_ary)
+        df = pd.DataFrame(te_ary, columns=te.columns_)
+        # print(df)
+        frequent_itemsets = apriori(df, min_support=MIN_SUPP, use_colnames=True)
+        listSupport = list(frequent_itemsets['support'])
+        listItemsets = list(frequent_itemsets['itemsets'])
+        # print(listSupport, listItemsets)
+        # addaverage(frequent_itemsets)
+        max = len(listSupport)
+        for i in range(0, max):
+            # print(listSupport[i], listItemsets[i])
+            if listItemsets[i] in dic:
+                dicListItemsets = dic[listItemsets[i]]
+                dic[listItemsets[i]] = {'support': dicListItemsets['support'] + listSupport[i], 'count': dicListItemsets['count'] + 1}
+            else:
+                dic[listItemsets[i]] = {'support': listSupport[i], 'count': 1}
+
+    # print(dic)
+    for key, value in dic.items():
+        print(f'{key} {value} - ', value['support']/value['count'])
+    return False
+
     sets = 0
-    for count in range(0, rowsCount + split, split):
+    dic = {}
+    for count in range(0, max, split):
         # print(count)
         sets += 1
 
@@ -37,9 +76,34 @@ def fis(dataset, split=1000):
         # print(te_ary.astype("int"))
         df = pd.DataFrame(te_ary, columns=te.columns_)
         # print(df)
-        frequent_itemsets = apriori(df, min_support=0.2, use_colnames=True)
-        addaverage(frequent_itemsets)
+        frequent_itemsets = apriori(df, min_support=0.6, use_colnames=True)
+        # addaverage(frequent_itemsets)
         print(frequent_itemsets)
+
+        for index, row in frequent_itemsets.iterrows():
+            itemsets = row['itemsets']
+            support = row['support']
+            print(support, itemsets)
+            if itemsets in dic:
+                print('exist')
+                # take average
+                oldSupport = dic[itemsets]
+                avgSupport = (oldSupport + support) / 2
+                dic[itemsets] = avgSupport
+            else:
+                dic[itemsets] = {'support': support, count: 1}
+            print(dic)
+
+
+def itemset(dataset):
+    global MIN_SUPP
+    te = TransactionEncoder()
+    te_ary = te.fit(dataset).transform(dataset)
+    # print(te_ary)
+    df = pd.DataFrame(te_ary, columns=te.columns_)
+    # print(df)
+    frequent_itemsets = apriori(df, min_support=MIN_SUPP, use_colnames=True)
+    print(frequent_itemsets)
 
 
 # Apple(A) Banana(B) Carrot(C) Dill(D) Ember(E)
@@ -49,10 +113,10 @@ dataset = [['A', 'B'],
            ['A', 'B', 'E']]
 # Open dataset from file
 fileName = './kosarak.dat'
-lineList = [(line.rstrip('\n')).split(' ') for line in open(fileName) if not line.isspace()]
+# lineList = [(line.rstrip('\n')).split(' ') for line in open(fileName) if not line.isspace()]
 # print(lineList[0:15])
-
-dataset = lineList[0:1000]
-print(f'Number of rows : {len(lineList)}')
+# dataset = lineList[0:2000]
+# print(f'Number of rows : {len(lineList)}')
 print('Dataset')
-fis(dataset)
+itemset(dataset)
+fis(dataset, 2)
